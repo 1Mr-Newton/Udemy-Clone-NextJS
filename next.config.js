e3f9ad7eb4284f72723e30630c851f897fdac690
1mr-newton <eaidoo015@gmail.com>
2023-09-25 18:03:50 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["img-c.udemycdn.com", "img-b.udemycdn.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
